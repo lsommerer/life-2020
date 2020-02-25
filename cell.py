c48f6bf89eb4751c79251c981b1b3a5c3229bed5
Lloyd Sommerer <lloyd@lloydandlauren.com>
2020-02-25 12:38:26 -0600

    Changed instance variables from private to protected.

--- cell.py
+++ cell.py
@@ -45,10 +45,10 @@ class Cell(object):
         """Given a row and a column, creates a cell that knows its row,
            column, living (all cells start off with living as False), and
            neighbors (all cells start off with an empty list for neighbors)."""
-        self.__row = row
-        self.__column = column
+        self._row = row
+        self._column = column
         self.living = False
-        self.__neighbors = []
+        self._neighbors = []
 
     def __str__(self):
         """Returns either the liveChar or the deadChar for the Cell class
@@ -70,10 +70,10 @@ class Cell(object):
             raise TypeError('state must be boolean.')
 
     def get_row(self):
-        return self.__row
+        return self._row
 
     def get_column(self):
-        return self.__column
+        return self._column
 
     def add_neighbor(self, cell):
         #
@@ -81,11 +81,11 @@ class Cell(object):
         # out you know all the neighbors are working.
         #
         #print(f'{self.__repr__()} add neighbor {cell.__repr__()}')
-        self.__neighbors.append(cell)
+        self._neighbors.append(cell)
 
     def living_neighbors(self):
         neighborCount = 0
-        for neighbor in self.__neighbors:
+        for neighbor in self._neighbors:
             if neighbor.get_living() == True:
                 neighborCount += 1
         return neighborCount
@@ -95,12 +95,12 @@ class Cell(object):
         # Here's a handy way to use if..else that we haven't talked about.
         #
         state = 'alive' if self.living else 'dead'
-        return f'Cell({self.__row},{self.__column}) [{state}]'
+        return f'Cell({self._row},{self._column}) [{state}]'
 
     def debug(self):
         """Sometimes you just need to know about a cell."""
-        neighbors = len(self.__neighbors)
+        neighbors = len(self._neighbors)
         string = self.__repr__() + f' neighbors: {self.living_neighbors()}/{neighbors}'
-        for neighbor in self.__neighbors:
+        for neighbor in self._neighbors:
             string += '\n     ' + neighbor.__repr__()
         print(string)
